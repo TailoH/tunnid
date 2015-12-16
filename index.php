58d19115a86d8c8cdae787e5d7f0afa3096909ad
TailoH <tailo122@gmail.com>
2015-12-16 09:37:52 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<a href="form.html">Vormi sisestamine</a><br>
+<a href="one.php">Ühe sisestuse kuvamine</a><br>
+<p>
+  <?php
+  echo'<center>';
+  echo'<table width="400">
+  <tr>
+  <td bgcolor="darkgray"><b>Nimi</b></td>
+  <td bgcolor="gray"><b>Detailne</b></td>
+  </tr>';
+  echo'</center>';
+  ?>
+</p>
+
