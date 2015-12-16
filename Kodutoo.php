d9b71b30c8be37508af42b697e29761e183a21f5
TailoH <tailo122@gmail.com>
2015-12-16 09:36:31 +0200

    Create Kodutoo.php

---
+++ Kodutoo.php
@@ -0,0 +1,113 @@
+Tailo
+Drive
+.
+Kausta tee
+Minu Drive
+III kursus
+Süsteemprogrammeerimine
+Kaustad ja kuvad
+Minu Drive
+Minuga jagatud
+Google’i fotod
+Hiljutised
+Tärniga tähistatud
+Prügikast
+717 MB / 15 GB-st on kasutuselTäiendage salvestusruumi
+Nimi
+Omanik
+Viimati muudetud
+Faili suurus
+vorm.php
+mina
+9:26mina
+2 kB
+tailo.php
+mina
+9:26mina
+966 baiti
+saatmine2.php
+mina
+9:26mina
+469 baiti
+one.php
+mina
+9:26mina
+5 baiti
+Kodutoo2.php
+mina
+9:26mina
+1 kB
+index2.php
+mina
+9:26mina
+283 baiti
+index.php
+mina
+9:26mina
+318 baiti
+data.txt
+mina
+9:26mina
+—
+
+<!DOCTYPE html>
+<body>
+<html>
+<!-- Text input -->
+<form action="tailo.php" method="POST" enctype="multipart/form-data">
+<h2>Text input</h2>
+  Kasutaja:<br>
+  <input type="text" name="kasutaja">
+  <br>
+  Parool:<br>
+  <input type="text" name="parool">
+
+<!-- Textarea -->
+
+<h2>Textarea</h2>
+<textarea rows="4" cols="50" name="textarea">
+ 
+</textarea>
+
+<!-- Checkboxes -->
+
+<h2>Checkboxes</h2>
+
+<input type="checkbox" name="sõiduk" value="Ratas">Mul on ratas<br>
+<input type="checkbox" name="sõiduk" value="Auto">Mul on auto 
+
+<!-- Radio buttons -->
+
+<h2>Radio buttons</h2>
+
+<input type="radio" name="sex" value="male">Male<br>
+<input type="radio" name="sex" value="female">Female
+
+
+<!-- Select tag -->
+
+<h2>Select tag</h2>
+<select name="auto">
+  <option  value="volvo">Volvo</option> 
+  <option  value="saab">Saab</option>
+  <option  value="mercedes">Mercedes</option>
+  <option  value="audi">Audi</option>
+</select>
+
+
+<!-- Image -->
+
+<h2>Pildi pealkiri</h2>
+<input type="file" name="fileToUpload" id="fileToUpload"><br>
+
+<!-- Submit button -->
+
+<h2> Submit button </h2>
+  
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>
+
+Kodutoo2.php
+Üksuse Kodutoo2.php kuvamine.
