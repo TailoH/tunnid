8993770fd1553348e7f740cead93d23afa17fce7
TailoH <tailo122@gmail.com>
2015-12-16 09:39:16 +0200

    Update and rename Kodutoo.php to vorm.php

--- vorm.php
+++ vorm.php
@@ -1,55 +1,3 @@
-Tailo
-Drive
-.
-Kausta tee
-Minu Drive
-III kursus
-Süsteemprogrammeerimine
-Kaustad ja kuvad
-Minu Drive
-Minuga jagatud
-Google’i fotod
-Hiljutised
-Tärniga tähistatud
-Prügikast
-717 MB / 15 GB-st on kasutuselTäiendage salvestusruumi
-Nimi
-Omanik
-Viimati muudetud
-Faili suurus
-vorm.php
-mina
-9:26mina
-2 kB
-tailo.php
-mina
-9:26mina
-966 baiti
-saatmine2.php
-mina
-9:26mina
-469 baiti
-one.php
-mina
-9:26mina
-5 baiti
-Kodutoo2.php
-mina
-9:26mina
-1 kB
-index2.php
-mina
-9:26mina
-283 baiti
-index.php
-mina
-9:26mina
-318 baiti
-data.txt
-mina
-9:26mina
-—
-
 <!DOCTYPE html>
 <body>
 <html>
@@ -109,5 +57,4 @@ mina
 </body>
 </html>
 
-Kodutoo2.php
-Üksuse Kodutoo2.php kuvamine.
+
